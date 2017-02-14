1afdeb2814e4a9f2f2b592a0092eaa8f5e3db602
Óscar repiso <scar repiso>
2017-02-14 08:39:57 +0100

    actualizacion cambios

--- VirtualCoach/src/main/java/model/Entrenador.java
+++ VirtualCoach/src/main/java/model/Entrenador.java
@@ -1,8 +1,10 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.*;
+
 
 
 @Entity
@@ -13,7 +15,7 @@ public class Entrenador extends Usuario{
 	private long id_coach;
 	//Un entrenador tiene varios clientes. Se guarda en cliente
 	@OneToMany(mappedBy="entrenador")
-	private ArrayList <Cliente> clientes;
+	private List <Cliente> clientes;
 	
 
 	public Entrenador(){
